fc10b7be97d3f5b34ff0a39ba9d322f49d7c26e3
Daniel <d.andres.bacho@gmail.com>
2024-04-08 10:12:28 +0200

    Initial commit

---
+++ src/test/java/org/example/demoreadfileslocal/DemoReadFilesLocalApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.demoreadfileslocal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoReadFilesLocalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
